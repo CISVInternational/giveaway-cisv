31e02ecfaefd9a0ce4431bf7c5587cd07e35eccd
Rulo Alonso <r.alonso.lopez@gmail.com>
2021-11-04 21:33:16 +0100

    redux data al data export component

--- src/containers/DataExport/DataExport.tsx
+++ src/containers/DataExport/DataExport.tsx
@@ -1,6 +1,15 @@
+import { useSelector } from "react-redux"
+import { getDestinies, getParticipants, getPrograms } from "../../redux/selectors/general.selector"
 import "./DataExport.css"
 
 const DataExport = () => {
+
+  const participants = useSelector(getParticipants)
+  const programs = useSelector(getPrograms)
+  const destinies = useSelector(getDestinies)
+  console.log(participants);
+  console.log(programs);
+
   return (
     <div>
       Data Export
