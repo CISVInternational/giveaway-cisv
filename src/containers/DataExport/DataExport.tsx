a630e05dd39d5ffd6653f690cbbf8be59e61a627
Rulo Alonso <r.alonso.lopez@gmail.com>
2021-12-11 13:03:57 +0100

    xlsx hello world

--- src/containers/DataExport/DataExport.tsx
+++ src/containers/DataExport/DataExport.tsx
@@ -1,14 +1,41 @@
 import { useSelector } from "react-redux"
-import { getDestinies, getParticipants, getPrograms } from "../../redux/selectors/general.selector"
+import { getPrograms } from "../../redux/selectors/general.selector"
 import "./DataExport.css"
+import XLSX from "xlsx"
+import { saveAs } from 'file-saver';
 
 const DataExport = () => {
 
-  const participants = useSelector(getParticipants)
   const programs = useSelector(getPrograms)
-  const destinies = useSelector(getDestinies)
-  console.log(participants);
   console.log(programs);
+
+  let wb = XLSX.utils.book_new();
+
+  wb.Props = {
+    Title: "SheetJS Workbook",
+    Subject: "Test",
+    Author: "CISV International Dev Team",
+    CreatedDate: new Date()
+  };
+
+  wb.SheetNames.push("Test Sheet");
+  var ws_data = [['hello' , 'world']];
+  var ws = XLSX.utils.aoa_to_sheet(ws_data);
+  wb.Sheets["Test Sheet"] = ws;
+  var wbout = XLSX.write(wb, {bookType:'xlsx',  type: 'binary'});
+
+  function s2ab(s: string) { 
+    var buf = new ArrayBuffer(s.length); //convert s to arrayBuffer
+    var view = new Uint8Array(buf);  //create uint8array as viewer
+    for (var i=0; i<s.length; i++) view[i] = s.charCodeAt(i) & 0xFF; //convert to octet
+    return buf;    
+  }
+
+  function downloadSheet() {
+    saveAs(new Blob([s2ab(wbout)],{type:"application/octet-stream"}), 'test.xlsx');
+  }
+
+  downloadSheet();
 
   return (
     <div>
